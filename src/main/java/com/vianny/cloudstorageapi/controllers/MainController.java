7c9362238e2cad708fe3eff8d4bf28dc897c72d7
ilya <ilya173.picenko36.95@mail.ru>
2024-03-08 22:15:23 +0300

    implementation of deletion of s3 file from storage

--- src/main/java/com/vianny/cloudstorageapi/controllers/MainController.java
+++ src/main/java/com/vianny/cloudstorageapi/controllers/MainController.java
@@ -3,13 +3,18 @@ package com.vianny.cloudstorageapi.controllers;
 import com.vianny.cloudstorageapi.config.MinioConfig;
 import com.vianny.cloudstorageapi.dto.ResponseMessage;
 import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+import io.minio.errors.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.Principal;
 
 @RestController
@@ -22,11 +27,11 @@ public class MainController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam MultipartFile file, @RequestParam String directory, Principal principal) {
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam MultipartFile object, @RequestParam String directory, Principal principal) {
         try {
-            String objectName = directory + "/" + file.getOriginalFilename();
+            String objectName = directory + "/" + object.getOriginalFilename();
 
-            InputStream inputStream = file.getInputStream();
+            InputStream inputStream = object.getInputStream();
             minioConfig.minioClient().putObject(PutObjectArgs.builder()
                     .bucket(principal.getName())
                     .object(objectName)
@@ -39,5 +44,24 @@ public class MainController {
 
         ResponseMessage responseMessage = new ResponseMessage(HttpStatus.CREATED, "Файл успешно загружен");
         return new ResponseEntity<>(responseMessage, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{directory}")
+    public ResponseEntity<ResponseMessage> deleteFile(@PathVariable String directory, Principal principal) {
+        try {
+            RemoveObjectArgs removeObjectArgs = RemoveObjectArgs.builder()
+                    .bucket(principal.getName())
+                    .object(directory)
+                    .build();
+            minioConfig.minioClient().removeObject(removeObjectArgs);
+        }
+        catch (RuntimeException | ServerException | InsufficientDataException | ErrorResponseException | IOException |
+               NoSuchAlgorithmException | InvalidKeyException | InvalidResponseException | XmlParserException |
+               InternalException e) {
+            throw new RuntimeException(e);
+        }
+
+        ResponseMessage responseMessage = new ResponseMessage(HttpStatus.OK, "Файл успешно удален");
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
     }
 }
