abbd9f0909c7311a72130808891b7644053d3825
ilya <ilya173.picenko36.95@mail.ru>
2024-03-08 21:09:14 +0300

    implementation of uploading to server and saving to s3 file

--- src/main/java/com/vianny/cloudstorageapi/controllers/MainController.java
+++ src/main/java/com/vianny/cloudstorageapi/controllers/MainController.java
@@ -2,6 +2,7 @@ package com.vianny.cloudstorageapi.controllers;
 
 import com.vianny.cloudstorageapi.dto.ResponseMessage;
 import com.vianny.cloudstorageapi.dto.request.RequestData;
+import com.vianny.cloudstorageapi.models.ObjectDetails;
 import com.vianny.cloudstorageapi.services.MinioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,5 +16,18 @@ import java.security.Principal;
 @RestController
 @RequestMapping("/myCloud")
 public class MainController {
+    private final MinioService minioService;
 
+    @Autowired
+    public MainController(MinioService minioService) {
+        this.minioService = minioService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam MultipartFile file, @RequestParam String directory, Principal principal) {
+        minioService.uploadFile(file, principal.getName(), directory);
+
+        ResponseMessage responseMessage = new ResponseMessage(HttpStatus.CREATED, "Файл успешно загружен");
+        return new ResponseEntity<>(responseMessage, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/vianny/cloudstorageapi/services/MinioService.java
+++ src/main/java/com/vianny/cloudstorageapi/services/MinioService.java
@@ -6,7 +6,10 @@ import io.minio.PutObjectArgs;
 import io.minio.errors.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -28,5 +31,19 @@ public class MinioService {
         );
     }
 
+    public void uploadFile(MultipartFile file, String username, String directory) {
+        try {
+            String objectName = directory + "/" + file.getOriginalFilename();
 
+            InputStream inputStream = file.getInputStream();
+            minioConfig.minioClient().putObject(PutObjectArgs.builder()
+                    .bucket(username)
+                    .object(objectName)
+                    .stream(inputStream, inputStream.available(), -1)
+                    .build());
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
