8500fa9d507382cc3d1fcc0ceb983b6136a33c0a
ilya <ilya173.picenko36.95@mail.ru>
2024-03-10 16:55:28 +0300

    refactor(sql): add column object_type

--- src/main/java/com/vianny/cloudstorageapi/sql/database-api.sql
+++ src/main/java/com/vianny/cloudstorageapi/sql/database-api.sql
@@ -8,11 +8,12 @@ CREATE TABLE account (
 );
 
 CREATE TABLE details_object (
-                              id SERIAL PRIMARY KEY,
-                              account_id INTEGER,
-                              object_name text not null,
-                              object_size INTEGER not null,
-                              object_location text not null,
-                              upload_date timestamp not null,
-                              FOREIGN KEY (account_id) REFERENCES account(id)
+    id SERIAL PRIMARY KEY,
+    account_id INTEGER,
+    object_name text not null,
+    object_type text not null,
+    object_size INTEGER not null,
+    object_location text,
+    upload_date timestamp not null,
+    FOREIGN KEY (account_id) REFERENCES account(id)
 );
