93d74a2d56f982ac399105c0ffbe7bba5806f59c
ilya <ilya173.picenko36.95@mail.ru>
2024-03-16 18:43:18 +0300

    add(service): folderService

--- src/main/java/com/vianny/cloudstorageapi/services/FolderService.java
+++ src/main/java/com/vianny/cloudstorageapi/services/FolderService.java
@@ -1,18 +1,41 @@
 package com.vianny.cloudstorageapi.services;
 
+import com.vianny.cloudstorageapi.enums.TypeObject;
+import com.vianny.cloudstorageapi.exception.requiredException.ConflictRequiredException;
+import com.vianny.cloudstorageapi.models.Account;
+import com.vianny.cloudstorageapi.models.ObjectDetails;
+import com.vianny.cloudstorageapi.repositories.AccountRepository;
 import com.vianny.cloudstorageapi.repositories.ObjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 public class FolderService {
     private ObjectRepository objectRepository;
+    private AccountRepository accountRepository;
     @Autowired
     public void setFileRepository(ObjectRepository objectRepository) {
         this.objectRepository = objectRepository;
     }
+    @Autowired
+    public void setAccountRepository(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 
-    private void saveFolder() {
+    private void saveFolder(String folderName, String path, String login) {
+        ObjectDetails objectDetails = new ObjectDetails();
+        Optional<Account> currentAccount = accountRepository.findUserByLogin(login);
 
+        objectDetails.setObjectName(folderName);
+        objectDetails.setObjectType(TypeObject.Folder);
+        objectDetails.setObjectSize(0);
+        objectDetails.setObjectLocation(path);
+        objectDetails.setUploadDate(LocalDateTime.now());
+        objectDetails.setAccount(currentAccount.orElseThrow());
+
+        objectRepository.save(objectDetails);
     }
 }
