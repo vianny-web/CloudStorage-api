1322e47dad319b46863ef597da098c4b9ede4990
ilya <ilya173.picenko36.95@mail.ru>
2024-03-22 22:34:56 +0300

    add(test): reduceSizeStorage method

---
+++ src/test/java/com/vianny/cloudstorageapi/services/AccountServiceTest.java
@@ -0,0 +1,67 @@
+package com.vianny.cloudstorageapi.services;
+
+import com.vianny.cloudstorageapi.dto.AccountDTO;
+import com.vianny.cloudstorageapi.exception.requiredException.NoStorageSpaceRequiredException;
+import com.vianny.cloudstorageapi.repositories.AccountRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountServiceTest {
+
+    @Mock
+    private AccountRepository accountRepository;
+    @InjectMocks
+    private AccountService accountService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void shouldReturnAccountDetailsSuccessfully() {
+        List<AccountDTO> expectedAccountDTOList = new ArrayList<>();
+        expectedAccountDTOList.add(new AccountDTO("user", 1000));
+
+        when(accountRepository.getAccountDetailsByLogin("user")).thenReturn(expectedAccountDTOList);
+
+        List<AccountDTO> actualAccountDTOList = accountService.getAccountDetails("user");
+
+        assertEquals(expectedAccountDTOList, actualAccountDTOList);
+    }
+
+    @Test
+    void shouldReduceSizeStorageSuccessfullyWhenSpaceIsAvailable() {
+        when(accountRepository.findSizeStorageByLogin("test1")).thenReturn(10000);
+        when(accountRepository.findSizeStorageByLogin("test2")).thenReturn(1000);
+
+        accountService.reduceSizeStorage("test1", 1000);
+        verify(accountRepository).subtractBytesFromSizeStorage("test1", 1000);
+
+        accountService.reduceSizeStorage("test2", 1000);
+        verify(accountRepository).subtractBytesFromSizeStorage("test2", 1000);
+    }
+
+    @Test
+    void shouldThrowNoStorageSpaceRequiredExceptionWhenSpaceIsNotAvailable() {
+        when(accountRepository.findSizeStorageByLogin("test")).thenReturn(100);
+
+        assertThrows(NoStorageSpaceRequiredException.class, () -> {
+            accountService.reduceSizeStorage("test", 1000);
+        });
+    }
+}
+
