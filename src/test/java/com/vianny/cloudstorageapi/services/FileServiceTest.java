4afce5eeb9ba604e75f968292a13ba0ae647748f
ilya <ilya173.picenko36.95@mail.ru>
2024-03-30 14:43:55 +0300

    add(test): deleteFile

--- src/test/java/com/vianny/cloudstorageapi/services/FileServiceTest.java
+++ src/test/java/com/vianny/cloudstorageapi/services/FileServiceTest.java
@@ -2,6 +2,7 @@ package com.vianny.cloudstorageapi.services;
 
 import com.vianny.cloudstorageapi.enums.TypeObject;
 import com.vianny.cloudstorageapi.exception.requiredException.ConflictRequiredException;
+import com.vianny.cloudstorageapi.exception.requiredException.NotFoundRequiredException;
 import com.vianny.cloudstorageapi.models.Account;
 import com.vianny.cloudstorageapi.models.ObjectDetails;
 import com.vianny.cloudstorageapi.repositories.AccountRepository;
@@ -15,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -31,16 +33,17 @@ public class FileServiceTest {
 
     Principal principal;
     String fullDirectory;
-    String fileName;
+    String filename;
     MockMultipartFile mockFile;
 
     @BeforeEach
     void setUp() {
         principal = () -> "user-1";
         fullDirectory = principal.getName() + "/" + "files/";
-        fileName = "file.txt";
-        mockFile = new MockMultipartFile("file", fileName, "", "Some file content".getBytes());
+        filename = "file.txt";
+        mockFile = new MockMultipartFile("file", filename, "", "Some file content".getBytes());
     }
+
 
     // Тестирование всех случаев метода "saveFile"
     @Test
@@ -48,7 +51,7 @@ public class FileServiceTest {
         Account account = new Account(principal.getName(), anyString());
 
         when(accountRepository.findUserByLogin(principal.getName())).thenReturn(Optional.of(account));
-        when(objectRepository.findByObjectNameAndObjectTypeAndObjectLocationAndAccount_Login(fileName, TypeObject.File, fullDirectory, principal.getName())).thenReturn(null);
+        when(objectRepository.findByObjectNameAndObjectTypeAndObjectLocationAndAccount_Login(filename, TypeObject.File, fullDirectory, principal.getName())).thenReturn(null);
 
         fileService.saveFile(mockFile, fullDirectory, principal.getName());
 
@@ -56,8 +59,27 @@ public class FileServiceTest {
     }
     @Test
     void testSaveFile_ConflictRequiredException() {
-        when(objectRepository.findByObjectNameAndObjectTypeAndObjectLocationAndAccount_Login(fileName, TypeObject.File, fullDirectory, principal.getName())).thenReturn(new ObjectDetails());
+        when(objectRepository.findByObjectNameAndObjectTypeAndObjectLocationAndAccount_Login(filename, TypeObject.File, fullDirectory, principal.getName())).thenReturn(new ObjectDetails());
 
         assertThrows(ConflictRequiredException.class,() -> fileService.saveFile(mockFile, fullDirectory, principal.getName()));
+    }
+
+
+    // Тестирование всех случаев метода "deleteFile"
+    @Test
+    void testDeleteFile() {
+        ObjectDetails objectDetails = new ObjectDetails(filename, TypeObject.File, 100, fullDirectory, LocalDateTime.now());
+
+        when(objectRepository.findByObjectNameAndObjectTypeAndObjectLocationAndAccount_Login(filename, TypeObject.File, fullDirectory, principal.getName())).thenReturn(objectDetails);
+
+        fileService.deleteFile(filename, fullDirectory, principal.getName());
+
+        verify(objectRepository, times(1)).deleteByObjectLocation(objectDetails.getObjectName(), objectDetails.getObjectType(), objectDetails.getObjectLocation(), principal.getName());
+    }
+    @Test
+    void testDeleteFile_NotFoundRequiredException() {
+        when(objectRepository.findByObjectNameAndObjectTypeAndObjectLocationAndAccount_Login(filename, TypeObject.File, fullDirectory, principal.getName())).thenReturn(null);
+
+        assertThrows(NotFoundRequiredException.class,() -> fileService.deleteFile(filename, fullDirectory, principal.getName()));
     }
 }
