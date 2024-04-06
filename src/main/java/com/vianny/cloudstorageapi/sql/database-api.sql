4e1cae30d358fb325b8be93ae8321bbdd93808c3
ilya <ilya173.picenko36.95@mail.ru>
2024-04-06 19:10:20 +0300

    refactor(sql): request separation

--- src/main/java/com/vianny/cloudstorageapi/sql/database-api.sql
+++ src/main/java/com/vianny/cloudstorageapi/sql/database-api.sql
@@ -1,19 +1,1 @@
 CREATE database cloud_storage_db;
-
-CREATE TABLE account (
-                         id SERIAL PRIMARY KEY,
-                         login text not null,
-                         password text not null,
-                         size_storage INTEGER
-);
-
-CREATE TABLE details_object (
-    id SERIAL PRIMARY KEY,
-    account_id INTEGER,
-    object_name text not null,
-    object_type text not null,
-    object_size INTEGER not null,
-    object_location text,
-    upload_date timestamp not null,
-    FOREIGN KEY (account_id) REFERENCES account(id)
-);
